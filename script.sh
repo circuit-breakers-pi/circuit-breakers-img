399e38645d9cd7c155f2bc5d2ed1fdadca1c7de8
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-12 14:46:04 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,29 +1,29 @@
-#---------webapp bestanden binnenhalen----------------
-sudo mkdir /home/pi/project
-
-sudo mkdir /home/pi/project/download
-
 cd /home/pi/project/download
 rm -rf /home/pi/project/download/*
 
-mkdir /home/pi/project/download/api
-cd /home/pi/project/download/api
-wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/api/*
+#mkdir /home/pi/project/download/api
+#cd /home/pi/project/download/api
+#wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/api/*
+sudo git clone https://github.com/stefmarien95/backend_pcfruit.git
 
 
-cd /home/pi/project/download
+#cd /home/pi/project/download
 
-mkdir /home/pi/project/download/assets
-cd /home/pi/project/download/assets
-wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/assets/*
+#mkdir /home/pi/project/download/assets
+#cd /home/pi/project/download/assets
+#wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/assets/*
+
+sudo git clone https://github.com/maartenschroons/BuildCircuitFrontend
+
+#cd /home/pi/project/download
+#wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/*
 
 
-cd /home/pi/project/download
-wget ftp://ftpuser:breakers@192.168.0.77/ftp/project/*
 
+sudo rm -rf /var/www/html/*
+sudo mv -f /home/pi/project/download/backend_pcfruit/* /var/www/html/
 
+sudo mv -f /home/pi/project/download/BuildCircuitFrontend/CircuitFrontend/* /var/www/html
+#sudo mv -f /home/pi/project/download/BuildCircuitFrontend/CircuitFrontend/assets /var/www/html
 
-rm -rf /var/www/html/*
-
-mv -f /home/pi/project/download/* /var/www/html/
-
+#sudo mv -f /home/pi/project/download/* /var/www/html/
