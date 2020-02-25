184ecbd4a2cd8e9f649bea2b4780814b22e48d3c
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-25 15:41:49 +0100

    Update build_builder.sh

--- build_builder.sh
+++ build_builder.sh
@@ -5,6 +5,12 @@ sudo cat ./build_deel1 > main-es2015.js
 echo "Wat is het ip adres van de server?"
 read ip
 
-echo "const baselink = 'http://$ip/api/';" > main-es2015.js
+echo "      this.iframe = 'http://$ip:3000/d/76B5JFZRz/vinificatie?orgId=1&refresh=5m&from=now-7d&to=now&theme=light&kiosk=tv&var-vat=' + this.id;" >> main-es2015.js
 
+sudo echo cat ./build_baseline_iframe >> main-es2015.js
+
+echo "const baselink = 'http://$ip/api/';" >> main-es2015.js
 sudo cat ./build_deel2 > main-es2015.js
+
+
+echo " proces.iframe = "http://192.168.0.105:3000/d-solo/76B5JFZRz/vinificatie?orgId=1&refresh=5m&panelId=10&var-vat=" + proces.vatId;" >> main-es2015.js
