a5577076c02eef9f9a047bd2e615278f52368238
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-26 09:18:35 +0100

    Update build_builder.sh

--- build_builder.sh
+++ build_builder.sh
@@ -15,3 +15,15 @@ sudo cat ./build_this_process >> main-es2015.js
 
 echo " proces.iframe = "http://192.168.0.105:3000/d-solo/76B5JFZRz/vinificatie?orgId=1&refresh=5m&panelId=10&var-vat=" + proces.vatId;" >> main-es2015.js
 sudo cat ./build_process_einde >> main-es2015.js
+
+touch ./database.php
+echo "<?php" > ./database.php
+echo "class Database{ " >> ./database.php
+
+echo "private $host = '$ip';" >> ./database.php
+sudo cat ./api_deel2 >> ./database.php
+
+sudo mv ./database.php /var/www/html/api/config/database.php
+
+sudo service apache2 restart
+
