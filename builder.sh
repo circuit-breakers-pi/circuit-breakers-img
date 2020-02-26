8ff71101c1c3ed8b417ce7699edd9337603d5397
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-26 09:43:31 +0100

    Update and rename script.sh to builder.sh

--- builder.sh
+++ builder.sh
@@ -81,6 +81,40 @@ sudo mv -f $pad /var/www/html
 #sudo mv -f /home/pi/project/download/* /var/www/html/
 
 sudo rm /var/www/html/main-es2015.js
-sudo bash ./build_builder.sh
+
+
+
+#!bin/bash
+touch ./main-es2015.js
+sudo cat ./build_begin_const > main-es2015.js
+
+echo "Wat is het ip adres van de server?"
+read ip
+echo "const baselink = 'http://$ip/api/';" >> main-es2015.js
+
+sudo cat ./build_baselink_iframe >> main-es2015.js
+echo "      this.iframe = 'http://$ip:3000/d/76B5JFZRz/vinificatie?orgId=1&refresh=5m&from=now-7d&to=now&theme=light&kiosk=tv&var-vat=' + this.id;" >> main-es$
+
+
+sudo cat ./build_this_process >> main-es2015.js
+
+
+echo " proces.iframe = 'http://$ip:3000/d-solo/76B5JFZRz/vinificatie?orgId=1&refresh=5m&panelId=10&var-vat=' + proces.vatId;" >> main-es2015.js
+sudo cat ./build_process_einde >> main-es2015.js
+
+sudo mv ./main-es2015.js /var/www/html/main-es2015.js
+
+touch ./database.php
+echo "<?php" > ./database.php
+echo "class Database{ " >> ./database.php
+
+echo 'private $host ="'$ip'";' >> ./database.php
+
+sudo cat ./api_deel2 >> ./database.php
+
+sudo mv ./database.php /var/www/html/api/config/database.php
+
+
+
 sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
 sudo service apache2 restart
