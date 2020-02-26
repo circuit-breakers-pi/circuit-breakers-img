213563e8244d5bf39871c38dcfb0c2a3b862b88e
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-26 09:31:36 +0100

    Update build_builder.sh

--- build_builder.sh
+++ build_builder.sh
@@ -13,7 +13,7 @@ echo "      this.iframe = 'http://$ip:3000/d/76B5JFZRz/vinificatie?orgId=1&refre
 sudo cat ./build_this_process >> main-es2015.js
 
 
-echo " proces.iframe = "http://192.168.0.105:3000/d-solo/76B5JFZRz/vinificatie?orgId=1&refresh=5m&panelId=10&var-vat=" + proces.vatId;" >> main-es2015.js
+echo " proces.iframe = 'http://$ip:3000/d-solo/76B5JFZRz/vinificatie?orgId=1&refresh=5m&panelId=10&var-vat=' + proces.vatId;" >> main-es2015.js
 sudo cat ./build_process_einde >> main-es2015.js
 
 touch ./database.php
