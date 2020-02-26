239c5bff01618bc89956684b37526671d98ca9b4
circuit-breakers-pi <60348529+circuit-breakers-pi@users.noreply.github.com>
2020-02-26 12:41:56 +0100

    Create measurements_change_ip.sh

---
+++ measurements_change_ip.sh
@@ -0,0 +1,10 @@
+sudo cat ./measurements_part1 > /home/pi/scripts/measurements.py
+
+echo What is the IP address of your server?
+read ip
+
+sudo echo "serverIp = '$ip'" >> /home/pi/scripts/measurements.py
+
+sudo cat ./measurements_part2 >> /home/pi/scripts/measurements.py
+
+sudo rm ./measurements_part*
